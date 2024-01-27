4169206c6b85b1fe9340634f40ff8acbfe770128
JH <jaden.hepburn@ontariotechu.net>
2024-01-27 01:04:21 -0500

    Update base_world.tscn, Main_Menu.gd, Main_Menu.tscn, and 5 more files

---
+++ EF_Game/Script/reset_button.gd
@@ -0,0 +1,25 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	disabled = true
+	modulate.a = 0
+	
+	await get_tree().create_timer(5.0).timeout # test
+	disabled = false
+	modulate.a = 255
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_pressed():
+	print("restart")
+	get_tree().reload_current_scene()
+	
+	pass # Replace with function body.
---
+++ EF_Game/Script/value_handler.gd
@@ -0,0 +1,47 @@
+extends Node
+
+#@export var health: int
+@export var coins: int
+@export var distance: int
+@export var time: float
+@export var gameState: bool
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#health = 3
+	coins = 0
+	distance = 0
+	time = 0
+	gameState = false
+pass # Replace with function body.
+
+func set_coins(amount: int):
+	coins = amount
+
+func set_distance(amount: int):
+	distance = amount
+	
+func set_time(amount: int):
+	time = amount
+
+func set_game_state(state: bool):
+	gameState = state
+
+
+
+func get_coins():
+	return coins
+
+func get_distance():
+	return distance
+	
+func get_time():
+	return time
+
+func get_game_state():
+	return gameState
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
